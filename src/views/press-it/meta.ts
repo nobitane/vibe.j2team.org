e0236dc04b43993adedb8c7ebef1295ef74d8e89
Vy Ho <96521788+hoyvoh@users.noreply.github.com>
2026-03-08 17:09:13 +0700

    feat(press-it): Add link and category (#96)
    * feat: press it
    
    * fix: change to routerLink & rm
    
    * fix: description and titles
    
    * fix: back to home button
    
    * feat: update meta category & link
    
    * fix: remove changes on other dirs

--- src/views/press-it/meta.ts
+++ src/views/press-it/meta.ts
@@ -4,6 +4,8 @@ const meta: PageMeta = {
   name: 'Press It',
   description: "A rhythm game. Let's see how far you can go!",
   author: 'hoyvoh',
+  category: 'game',
+  facebook: 'https://github.com/hoyvoh',
 }
 
 export default meta
